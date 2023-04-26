acfa8673d1abc205112b98e517ae7931f4831601
Bhavitha <36759871+k-bhavitha@users.noreply.github.com>
2023-04-26 17:26:01 +0530

    provider.tf

---
+++ provider.tf
@@ -0,0 +1,9 @@
+provider "azurerm" {
+  
+  subscription_id = "${var.subscriptionId}"
+  client_id       = "${var.clientId}"
+  client_secret   = "${var.clientSecret}"
+  tenant_id       = "${var.tenantId}"
+
+  version = "=1.44.0"
+}
