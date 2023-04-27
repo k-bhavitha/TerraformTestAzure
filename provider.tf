2887fb993842a2d150a541617b3972f1e9cfc0b5
Bhavitha <36759871+k-bhavitha@users.noreply.github.com>
2023-04-27 14:45:04 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,9 +1,9 @@
 provider "azurerm" {
   
-  subscription_id = "29c1ad9f-091e-48a2-9184-01a489833f34"
-  client_id       = "7a5abe4f-478a-42d5-9cf5-8bf41610e254"
-  client_secret   = "UD~8Q~jMYSV3n5U_Er~VIaTwSBn4JhSfVAy~XcQs"
-  tenant_id       = "8bcff170-9979-491e-8683-d8ced0850bad"
+  subscription_id = ""
+  client_id       = ""
+  client_secret   = ""
+  tenant_id       = ""
 
   version = "=1.44.0"
 }
